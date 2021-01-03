2e7b0e9875a97af41795f1bd8cc25283fcc49195
ampeo1 <56560579+ampeo1@users.noreply.github.com>
2021-01-03 15:26:29 +0300

    error

--- laba3/app/src/main/java/com/example/battleship/game/map/MapAdapter.java
+++ laba3/app/src/main/java/com/example/battleship/game/map/MapAdapter.java
@@ -32,10 +32,85 @@ public class MapAdapter extends RecyclerView.Adapter<MapAdapter.ViewHolder> {
     }
 
     @Override
-    public void onBindViewHolder(@NonNull final ViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull final MapAdapter.ViewHolder holder, final int position) {
+        holder.image.setOnDragListener(new View.OnDragListener() {
+            private boolean status = true;
+            @Override
+            public boolean onDrag(View v, DragEvent event) {
+
+                // Defines a variable to store the action type for the incoming event
+                final int action = event.getAction();
+
+                // Handles each of the expected events
+                switch(action) {
+                    case DragEvent.ACTION_DRAG_STARTED:
+                        Log.e("ACTION_DRAG_STARTED", String.valueOf(holder.image.getId()));
+                        return true;
+                    case DragEvent.ACTION_DRAG_ENTERED:
+                        // Applies a green tint to the View. Return true; the return value is ignored.
+                        holder.image.setColorFilter(Color.GREEN);
+
+                        // Invalidate the view to force a redraw in the new tint
+                        holder.image.invalidate();
+                        return true;
+
+                    case DragEvent.ACTION_DRAG_LOCATION:
+                        return true;
+
+                    case DragEvent.ACTION_DRAG_EXITED:
+                        holder.image.clearColorFilter();
+                        holder.image.setColorFilter(Color.RED);
+                        status = false;
+                        return true;
+
+                    case DragEvent.ACTION_DROP:
+
+                        // Gets the item containing the dragged data
+
+                        // Turns off any color tints
+                        holder.image.clearColorFilter();
+
+                        // Invalidates the view to force a redraw
+                        holder.image.invalidate();
+                        return status;
+
+                    case DragEvent.ACTION_DRAG_ENDED:
+                        // Turns off any color tinting
+                        holder.image.clearColorFilter();
+
+                        // Invalidates the view to force a redraw
+                        holder.image.invalidate();
+                        Log.e("ACTION_DRAG_ENDED",String.valueOf(event.getResult()));
+                        if (event.getResult()) {
+                            Log.e("ACTION_DRAG_ENDED", "зашёл");
+                            fields.get(position).setStatus(StatusField.SHIP);
+                            notifyDataSetChanged();
+                        }
+
+
+                        // Does a getResult(), and displays what happened.
+
+                        // returns true; the value is ignored.
+                        return true;
+
+                    // An unknown action type was received.
+                    default:
+                        Log.e("DragDrop Example","Unknown action type received by OnDragListener.");
+                        break;
+                }
+
+                return false;
+            }
+        });
         switch(fields.get(position).getStatus()){
             case EMPTY:
                 holder.image.setImageResource(R.drawable.empty_field);
+                Log.e("Constructor", "EMPTY");
+                break;
+            case SHIP:
+                holder.image.setImageResource(R.drawable.part_ship);
+                Log.e("Constructor", "SHIP");
+                break;
         }
     }
 
@@ -44,6 +119,8 @@ public class MapAdapter extends RecyclerView.Adapter<MapAdapter.ViewHolder> {
         return fields.size();
     }
 
+
+
     public class ViewHolder extends RecyclerView.ViewHolder {
 
         final ImageView image;
@@ -51,72 +128,6 @@ public class MapAdapter extends RecyclerView.Adapter<MapAdapter.ViewHolder> {
         ViewHolder(View view) {
             super(view);
             image = (ImageView)view.findViewById(R.id.image_field);
-            image.setOnDragListener(new View.OnDragListener() {
-                private boolean status = false;
-                @Override
-                public boolean onDrag(View v, DragEvent event) {
-
-                    // Defines a variable to store the action type for the incoming event
-                    final int action = event.getAction();
-
-                    // Handles each of the expected events
-                    switch(action) {
-                        case DragEvent.ACTION_DRAG_STARTED:
-                            break;
-                        case DragEvent.ACTION_DRAG_ENTERED:
-                            // Applies a green tint to the View. Return true; the return value is ignored.
-                            image.setColorFilter(Color.GREEN);
-
-                            // Invalidate the view to force a redraw in the new tint
-                            image.invalidate();
-                            status = true;
-                            break;
-
-                        case DragEvent.ACTION_DRAG_LOCATION:
-
-                            break;
-
-                        case DragEvent.ACTION_DRAG_EXITED:
-                            image.clearColorFilter();
-                            image.setColorFilter(Color.RED);
-                            status = false;
-                            break;
-
-                        case DragEvent.ACTION_DROP:
-
-                            // Gets the item containing the dragged data
-
-                            // Turns off any color tints
-                            image.clearColorFilter();
-
-                            // Invalidates the view to force a redraw
-                            image.invalidate();
-                            return status;
-
-                        case DragEvent.ACTION_DRAG_ENDED:
-                            if (event.getResult()) {
-                                image.setImageResource(R.drawable.part_ship);
-                            }
-                            // Turns off any color tinting
-                            image.clearColorFilter();
-
-                            // Invalidates the view to force a redraw
-                            image.invalidate();
-
-                            // Does a getResult(), and displays what happened.
-
-                            // returns true; the value is ignored.
-                            break;
-
-                        // An unknown action type was received.
-                        default:
-                            Log.e("DragDrop Example","Unknown action type received by OnDragListener.");
-                            break;
-                    }
-
-                    return true;
-                }
-            });
         }
     }
 }
